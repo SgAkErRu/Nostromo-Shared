35e534edccefaa3af32cd286dba7e4292635567d
SgAkErRu <sulmpx60@yandex.ru>
2021-09-29 05:43:25 +0400

    Новые типы для передачи файлов (начата имплементация TUS протокола)

---
+++ types/FileHandlerTypes.ts
@@ -0,0 +1,82 @@
+
+export type HttpMethod = "get" | "options" | "post" | "patch" | "head";
+
+export const enum FileHandlerConstants {
+    TUS_VERSION = "1.0.0",
+    FILES_ROUTE = "/files"
+}
+
+interface Dict<T>
+{
+    [key: string]: T | undefined;
+}
+
+// for request and response
+interface HttpHeaders extends Dict<string>
+{
+    /**
+     * The Upload-Offset request and response header indicates a byte offset within a resource. The value MUST be a non-negative integer.
+     */
+    "Upload-Offset"?: string;
+
+    /**
+     * The Upload-Length request and response header indicates the size of the entire upload in bytes. The value MUST be a non-negative integer.
+     */
+    "Upload-Length"?: string;
+
+    /**
+     * The Tus-Resumable header MUST be included in every request and response except for OPTIONS requests. The value MUST be the version of the protocol used by the Client or the Server.
+     * If the version specified by the Client is not supported by the Server, it MUST respond with the 412 Precondition Failed status and MUST include the Tus-Version header into the response. In addition, the Server MUST NOT process the request.
+     */
+    "Tus-Resumable"?: string;
+
+    /**
+     * Directives for caching mechanisms in both requests and responses.
+     */
+    "Cache-Control"?: string;
+};
+
+/** For request. */
+export interface IncomingHttpHeaders extends HttpHeaders
+{
+    /**
+     * The X-HTTP-Method-Override request header MUST be a string which MUST be interpreted as the request’s method by the Server, if the header is presented. The actual method of the request MUST be ignored. The Client SHOULD use this header if its environment does not support the PATCH or DELETE methods.
+     */
+    "X-HTTP-Method-Override"?: HttpMethod;
+    /**
+     * The Content-Type representation header is used to indicate the original media type of the resource (prior to any content encoding applied for sending).
+     */
+    "Content-Type"?: string;
+};
+
+/** For response. */
+export interface OutgoingHttpHeaders extends HttpHeaders
+{
+    /**
+     * The Tus-Version response header MUST be a comma-separated list of protocol versions supported by the Server. The list MUST be sorted by Server’s preference where the first one is the most preferred one.
+     */
+    "Tus-Version"?: string;
+
+    /**
+     * The Tus-Extension response header MUST be a comma-separated list of the extensions supported by the Server. If no extensions are supported, the Tus-Extension header MUST be omitted.
+     */
+    "Tus-Extension"?: string;
+
+    /**
+     * The Tus-Max-Size response header MUST be a non-negative integer indicating the maximum allowed size of an entire upload in bytes. The Server SHOULD set this header if there is a known hard limit.
+     */
+    "Tus-Max-Size"?: string;
+};
+
+export interface FileHandlerRequest
+{
+    method: HttpMethod;
+    path: string;
+    headers: IncomingHttpHeaders;
+};
+
+export interface FileHandlerResponse
+{
+    headers: OutgoingHttpHeaders;
+    statusCode: number;
+};
